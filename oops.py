761515d8f33a142ebd8c21be89f61c6a03283204
Sumyya Sadre Alam <sumyyasadre05@gmail.com>
2026-01-31 16:21:49 +0530

    Update oops.py

--- oops.py
+++ oops.py
@@ -80,3 +80,122 @@ student2 = Student('Vishakha', 12, 99, team2)
 # print(student2.team)
 student1.student_details()
 student2.student_details()
+
+# 4 features in OOPs 
+# Abstraction
+# Encapsulation
+# Inheritance
+# Polymorphism
+
+
+# Abstraction 
+# hiding unnecesary deatils from users through class, methods 
+
+class Student: # student class
+    def __init__(self, name, grade, percentage): 
+        self.name = name  # attribute 
+        self.grade = grade  
+        self.percentage = percentage 
+
+    def student_details(self): # method - abstraction
+        print(f"{self.name} is in class {self.grade}, with {self.percentage+2}%") # hidden from users
+
+# object - instance of class 
+student1 = Student('Madhav', 11, 96)
+student2 = Student('Vishakha', 12, 97)
+
+student2.student_details()
+
+
+# Encapsulation 
+# Restrict access to certain attributes or methods to protect data and enforce controlled access
+
+class Student: # student class
+    def __init__(self, name, grade, percentage): 
+        self.name = name  # attribute 
+        self.grade = grade  
+        self.__percentage = percentage # double underscore limits access
+
+    def get_percentage(self):
+        return self.__percentage 
+
+    def student_details(self): # method 
+        print(f"{self.name} is in class {self.grade}, with {self.percentage}%") 
+
+# object - instance of class 
+student1 = Student('Madhav', 11, 96)
+student2 = Student('Vishakha', 12, 97)
+
+print(student1.__percentage) # error
+print(student1.percentage) # error
+print(student2.get_percentage())
+
+
+# Inheritance 
+# allows one class (child) to reuse the prop and methods of another class (parent). 
+
+# parent class - baap
+class Student: # student class
+    def __init__(self, name, grade, percentage): 
+        self.name = name  # attribute 
+        self.grade = grade  
+        self.percentage = percentage 
+
+    def student_details(self): # method 
+        print(f"{self.name} is in class {self.grade}, with {self.percentage}%")
+
+# object - instance of class 
+student1 = Student('Madhav', 11, 96)
+student2 = Student('Vishakha', 12, 99)
+
+# child class - beta 
+class GraduateStudent(Student): # Graduatestudent child class inherit prop and methods from Student Parent class
+    def __init__(self, name, grade, percentage, stream): # old parameters from parent class and new parameters in child class
+        super().__init__(name, grade, percentage) # call parent class init
+        self.stream = stream # new attribute in child class 
+
+    def student_details(self):
+        super().student_details() # method inherit from parent class
+        print(f'Stream is {self.stream}')
+
+# object
+Grad_Student1 = GraduateStudent('Keshav', 12, 96, 'PCM')
+print(Grad_Student1.stream) 
+
+Grad_Student1.student_details()
+
+
+# Polymorphism
+# allows methods in difft classes to have same name but difft behaviour depending on objects 
+
+class Student: # student class
+    def __init__(self, name, grade, percentage): 
+        self.name = name  # attribute 
+        self.grade = grade  
+        self.percentage = percentage 
+
+    def student_details(self): # method 
+        print(f"{self.name} is in class {self.grade}, with {self.percentage}%")
+
+# object - instance of class 
+student1 = Student('Madhav', 11, 96)
+student2 = Student('Vishakha', 12, 99)
+
+# child class  
+class GraduateStudent(Student): 
+    def __init__(self, name, grade, percentage, stream): 
+        super().__init__(name, grade, percentage) 
+        self.stream = stream 
+
+    def student_details(self): # method
+        # print(f'{self.name} is in class {self.grade}, with {self.percentage}% and from stream {self.stream}')
+        print('same method with difft o/p')
+
+# object - Student class
+student1 = Student('Madhav', 11, 96)
+
+# object - GraduateStudent class
+Grad_Student1 = GraduateStudent('Keshav', 12, 96, 'PCM')
+
+student1.student_details()
+Grad_Student1.student_details()
